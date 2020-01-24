b8cf78f349ef7e3e10a6d5a25436755bef4c4435
matthew-ferreira <matthew.ferreira@me.com>
2020-01-24 16:50:11 -0500

    Part 1 Complete

--- Simulation1_BER.m
+++ Simulation1_BER.m
@@ -1,42 +1,70 @@
 clear all;close all;clc
 
-numIter = 10; %100;
+numIter = 100; %100;
 nSym = 1000;
 SNR_Vec = 0:2:16;
 lenSNR = length(SNR_Vec);
+numTrainingSymbols = 80;
 
-M = 256;
+M = 2;
 
-chan = 1;
-%chan = [1 .2 .4]; %Moderate ISI
+%chan = 1;
+chan = [1 .2 .4]; %Moderate ISI
 %chan = [0.227 0.460 0.688 0.460 0.227]; %Severe ISI
 
 berVec = zeros(numIter, lenSNR);
 
 for i = 1:numIter
     
-    msg = randi([0 M-1], 1, nSym);
+    %these would most likely be hardcoded
+    tSyms = randi([0 M-1], 1, numTrainingSymbols);
+    msg = randi([0 M-1], 1, nSym-numTrainingSymbols);
+    msg_sent = [tSyms msg];
     
     for j = 1:lenSNR
-        tx = qammod(msg,M);
+        if isequal(M,2)
+            tx = pskmod(msg_sent,M);
+        else
+            tx = qammod(msg_sent,M);
+        end
         
         if isequal(chan,1)
             txChan = tx;
         elseif isa(chan, 'channel.rayleigh')
             reset(chan)
-            txChan = filter(chan,tx)
+            txChan = filter(chan,tx);
         else
-            txChan = filter(chan,1,tx)
+            txChan = filter(chan,1,tx);
         end
         
         txNoisy = awgn(txChan, SNR_Vec(j), 'measured');
+        
         %add equalization
         
-        rx = qamdemod(txNoisy, M);
+        eq = comm.LinearEqualizer;
+        eq.ReferenceTap = 1;
         
-        rxMSG = rx;
+        if isequal(M,2)
+            eq.Constellation = pskmod([0 1],M);
+        else
+            eq.Constellation = qammod(0:M-1,M);
+        end
         
-        [~,berVec(i,j)] = biterr(msg, rxMSG);
+        %train on the first N symbols
+        txEqualized = eq(txNoisy',tx(1:numTrainingSymbols)')';
+        equalizedSamples = txEqualized(numTrainingSymbols+1:length(txEqualized));
+        
+        
+        %uncomment the following for the unequalized symbols
+        %equalizedSamples = txNoisy(numTrainingSymbols+1:length(txNoisy));
+        
+        if isequal(M,2)
+            rx = pskdemod(equalizedSamples,M);
+        else
+            rx = qamdemod(equalizedSamples, M);
+        end
+        
+        [~,berVec(i,j)] = biterr(msg, rx);
         
     end
 end
@@ -49,11 +77,23 @@ semilogy(SNR_Vec, ber)
 %Note: EbNO = SNR - 10log10(bits per symbol) + 10log10(samples per symbol)
 %From the matlab website
 
-berTheory = berawgn(SNR_Vec - 10*log10(log2(M)), 'qam', M);
+if isequal(M,2)
+    berTheory = berawgn(SNR_Vec, 'psk', M, 'nondiff');
+else
+    berTheory = berawgn(SNR_Vec - 10*log10(log2(M)), 'qam', M);
+end
 
-hold on
-semilogy(SNR_Vec, berTheory, 'r');
-legend('BER', 'Theoretical BER')
+if isequal(chan,1)
+    hold on
+    semilogy(SNR_Vec, berTheory, 'r');
+    legend('BER', 'Theoretical BER')
+end
+
 xlabel('SNR')
 ylabel('BER')
-title(append(num2str(M),'-QAM'))
+
+if isequal(M,2)
+    title('BPSK')
+else
+    title(append(num2str(M),'-QAM'))
+end
